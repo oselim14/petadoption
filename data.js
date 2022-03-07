099cbb22555e35d778f2d13136bbcb5f63c689ec
Tavi Selim <oselim14@gmail.com>
2022-03-07 09:03:40 +0100

    seed data

---
+++ data.js
@@ -0,0 +1,21 @@
+let data = [
+    {
+        id: 1, name: 'Snoopy', type: 'Dog', gender: 'Male', zipcode: 97205 
+    }, {
+        id: 2, name: 'Willow', type: 'Cat', gender: 'Female', zipcode: 97205 
+    }, {
+        id: 3, name: 'Yoda', type: 'Dog', gender: 'Neutered', zipcode: 90210 
+    }, {
+        id: 4, name: 'Lassie', type: 'Dog', gender: 'Spayed', zipcode: 97215 
+    }, {
+        id: 5, name: 'Fluffy', type: 'Rabbit', gender: 'Female', zipcode: 97215 
+    }, {
+        id: 6, name: 'Rhea', type: 'Cat', gender: 'Spayed', zipcode: 97215 
+    }, {
+        id: 7, name: 'Lese', type: 'Cat', gender: 'Spayed', zipcode: 90210 
+    }, {
+        id: 8, name: 'Rascal', type: 'Dog', gender: 'Male', zipcode: 97205 
+    }
+]
+
+module.exports = data;
