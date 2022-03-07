394b551efca26989c3d4315da73a6ed3ce06fb93
Tavi Selim <oselim14@gmail.com>
2022-03-07 09:12:26 +0100

    filter for zipcode

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // access data from the data file. data is an array of objects.
 let data = require('./data');
 
+function zipcodeFilter() {
+    let input = 97205
+    let filteredList = [];
+    for (let i=0; i<data.length; i++){
+        if (data[i].zipcode == input){
+            filteredList.push(data[i]);
+        }
+    }
+    console.log(filteredList);
+}
+zipcodeFilter();
