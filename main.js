7b699a6cd2206057bad6907fe1ee099fdb899a59
Tavi Selim <oselim14@gmail.com>
2022-03-07 09:48:17 +0100

    use readline to use userinput to filter

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
 // access data from the data file. data is an array of objects.
+const { read } = require('fs');
 let data = require('./data');
+//use readline to gain user input over what they want to filter for
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-function zipcodeFilter() {
-    let input = 97205
-    let filteredList = [];
-    for (let i=0; i<data.length; i++){
-        if (data[i].zipcode == input){
-            filteredList.push(data[i]);
-        }
-    }
-    console.log(filteredList);
-}
-zipcodeFilter();
+readline.question('What pets are you looking for? ', input => {
+    //regex for zipcode having only numbers
+    let zip = /^\d+$/;
+    //regex for type having only letters
+    let type = /^[A-Za-z]+$/;
 
-function typeFilter(){
-    let input = 'Dog';
-    let filteredList = [];
-    for (let i=0; i < data.length; i++){
-        if (data[i].type === input){
-            filteredList.push(data[i]);
+    //if input matches only numbers, then run the zipcode function
+    if (input.match(zip)) {
+        function zipcodeFilter() {
+            let filteredList = [];
+            //iterate through the data and see if data at that index's zipcode is equal to the input zipcode.
+            // if the zipcode is equal to the input, push this to the filtered list.
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].zipcode == input) {
+                    filteredList.push(data[i]);
+                }
+            }
+            console.log(filteredList);
+            //print out the filtered list
         }
+        //initiate the function
+        zipcodeFilter();
     }
-    console.log(filteredList);
-}
-typeFilter();
+
+    //if input matches only letters, then run the type filter
+    else if (input.match(type)) {
+        function typeFilter() {
+            let filteredList = [];
+            //iterate through the data and see if data at that index's type is equal to the input type.
+            // if the type is equal to the input, push this to the filtered list.
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].type === input) {
+                    filteredList.push(data[i]);
+                }
+            }
+            console.log(filteredList);
+            //print out the filtered list
+        }
+        //Initiate the function
+        typeFilter();
+    }
+
+    //otherwise try running bothfilter for the zipcode and type
+    else {
+        function bothFilter() {
+            let filteredList = [];
+            // iterate through data to see if the zipcode and type are the same as the input zipcode and type
+            for (let i = 0; i < data.length; i++) {
+                if (input.includes(data[i].zipcode) && input.includes(data[i].type)) {
+                    filteredList.push(data[i]);
+                }
+            }
+            //print the filtered list
+            console.log(filteredList);
+        }
+        //initiate the function
+        bothFilter();
+    }
+
+    readline.close();
+});
